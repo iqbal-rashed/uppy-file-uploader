a8c795bcf87b2002cab85db99f1ba9589622b02b
Rashed Iqbal <dev.rashediqbal@gmail.com>
2022-09-16 18:51:44 +0600

    second init

--- server/index.js
+++ server/index.js
@@ -2,9 +2,28 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const app = express();
+const companion = require("@uppy/companion");
+const session = require("express-session");
 
+app.use(
+    cors({
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
+    })
+);
+app.options(
+    "*",
+    cors({
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    })
+);
 app.use(express.json());
-app.use(cors());
+app.use(
+    session({
+        secret: "dddddddddddfslkjdfsdlkjkdfdfsd",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 const upload = multer({ dest: "uploads/" });
 
@@ -14,7 +33,36 @@ app.post("/upload", upload.array("my-file"), async (req, res) => {
     if (req.files.length == 0) {
         return res.status(400).send("File upload failed");
     }
-    res.status(200).send("File uploaded successfully");
+    res.status(200).send({ uploadUrl: "Hello Hi rashed iqbal" });
 });
 
-app.listen(7060);
+const options = {
+    providerOptions: {
+        drive: {
+            key: "700238736421-blqii7e0ivfq4p4cnfra6k9jbjituvto.apps.googleusercontent.com",
+            secret: "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",
+        },
+        dropbox: {
+            key: "jb0hx3758ihn8qi",
+            secret: "o9492ep2lpo76gc",
+        },
+    },
+    server: {
+        host: "localhost:7090",
+        protocol: "http",
+    },
+    filePath: "./uploads",
+    secret: "fjskljdfjlskjdjf",
+    debug: true,
+
+    streamingUpload: true,
+    allowLocalUrls: false,
+
+    corsOrigins: true,
+};
+
+app.use(companion.app(options));
+
+const server = app.listen(7090);
+
+companion.socket(server, options);
